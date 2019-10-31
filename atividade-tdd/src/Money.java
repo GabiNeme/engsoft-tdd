85b1b6bf699041453850ff164b6ad3fc383461a9
GabiNeme <gabineme@gmail.com>
2019-10-31 14:52:24 -0300

    Commit & Push 4

--- atividade-tdd/src/Money.java
+++ atividade-tdd/src/Money.java
@@ -1,6 +1,7 @@
 
 abstract class Money {
    protected int amount;
+   protected String currency;
    
    public boolean equals(Object object) {
 	      Money money = (Money) object;
@@ -8,12 +9,18 @@ abstract class Money {
    }
    
    static Money dollar(int amount)  {
-	      return new Dollar(amount);
-	   }
-	   
-   static Money franc(int amount) {
-	      return new Franc(amount);
+	      return new Dollar(amount, "USD");
    }
 	   
-   abstract Money times(int multiplier);  
+   static Money franc(int amount) {
+      return new Franc(amount, "CHF");
+   }
+   
+   Money(int amount, String currency) {
+	      this.amount = amount;
+	      this.currency = currency;
+   }
+	   
+   abstract Money times(int multiplier);
+   abstract String currency();  
 }
